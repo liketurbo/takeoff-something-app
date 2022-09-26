2d5a24092ba8512008806ac9952a0c0f209631e8
Rami Chasygov <4y9hx1bs1@mozmail.com>
2022-09-26 10:12:27 +0300

    feat: add footer at index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,13 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import { GetServerSidePropsContext } from "next";
 import { unstable_getServerSession } from "next-auth/next";
-import { useSession } from "next-auth/react";
 
-import AddContact from "../src/components/ContactDialog";
 import Contacts from "../src/components/Contacts";
 import Copyright from "../src/components/Copyright";
 import Header from "../src/components/Header";
 import { authOptions } from "./api/auth/[...nextauth]";
 
 export default function Index() {
-  const { data: session } = useSession();
-
   return (
     <>
       <Header />
@@ -20,6 +16,7 @@ export default function Index() {
         <Box sx={{ my: 4 }}>
           <Contacts />
         </Box>
+        <Copyright />
       </Container>
     </>
   );
